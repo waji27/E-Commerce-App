8c173dad1c6c4ec082a1c88c5c7adabf49880a37
Wajdan <kwaji4926@gmail.com>
2024-01-30 20:27:21 +0500

    working on this project for many days, now saving it on github for further work and collaborating with team.

---
+++ routes/authRoute.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//test routes
+router.post("/forgot-password", forgotPasswordController);
+
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+//protected route For admin
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+export default router;
